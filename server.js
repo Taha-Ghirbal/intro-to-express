449c28cb7e976e7a41630ab95e506d232813dbfa
Taha-Ghirbal <tahaghirbal@gmail.com>
2025-12-24 13:02:03 +0300

    lab finished

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
+
+app.get('/homepage', (req, res) => {
+  res.sendFile(__dirname + '/html/homepage.html');
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/html/about.html');
+})
+
+app.get('//test', (req, res) => {
+  res.send(<p>Success!</p>);
+}
